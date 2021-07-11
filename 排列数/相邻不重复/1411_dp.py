ad84381ea0632ff576f36921d78fe4ffdd076cd9
AI Era <47687653+boristown@users.noreply.github.com>
2021-07-11 13:09:48 +0800

    Update 1411_dp.py

--- 排列数/相邻不重复/1411_dp.py
+++ 排列数/相邻不重复/1411_dp.py
@@ -2,6 +2,8 @@ class Solution:
     def numOfWays(self, n: int) -> int:
         mod = 10**9 + 7
         fi0, fi1 = 6, 6
+        #fi0:ABC  = last( fi0*2 + fi1*2 )
+        #fi1:ABA  = last( fi0*2 + fi1*3 )
         for i in range(2, n + 1):
             fi0, fi1 = (2 * fi0 + 2 * fi1) % mod, (2 * fi0 + 3 * fi1) % mod
         return (fi0 + fi1) % mod
