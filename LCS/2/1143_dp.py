9583ef0916db8003a4acfa1d7f8c57a1e9f8f5b1
AI Era <47687653+boristown@users.noreply.github.com>
2021-07-26 09:20:42 +0800

    Update 1143_dp.py

--- LCS/2/1143_dp.py
+++ LCS/2/1143_dp.py
@@ -16,3 +16,5 @@ class Solution:
 #链接：https://leetcode-cn.com/leetbook/read/dynamic-programming-1-plus/5pkl9m/?discussion=mC1VwD
 #来源：力扣（LeetCode）
 #著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+#https://leetcode-cn.com/problems/longest-common-subsequence/
